e43c9120000cfde0055a64711bb8b7a34283bb84
falo10 <swiecinskiolaf@gmail.com>
2022-05-09 22:44:17 +0200

    adding 2nd function

--- myFuncts.py
+++ myFuncts.py
@@ -6,3 +6,21 @@ def show_keys_with_biggest_value (dictionary):
             for key, value in dictionary
             if (value == max(dictionary.values()))
             ]
+
+
+#2 With that function u can transform a list into conjunction of parametres -  e.g. exmp. id=5&id=10 if myList = [5, 10]
+def change_list_into_conj_of_param (myList, key = 'id'):
+
+    conj = ""
+    lastIteration = len(myList)
+    numberOfIteration = 0
+    
+    for item in myList:
+        numberOfIteration += 1
+        if (numberOfIteration == lastIteration):
+            conj += key + '=' + str(item)
+        else:
+            conj += key +'=' + str(item) + '&'
+
+
+    return conj
